6fd8822e5cd72715968e56e3f031590c927c2904
git3a <gr0342ir@ed.ritsumei.ac.jp>
2019-06-05 17:50:51 +0900

    new layout: layour_grid_item.xml(this is layout for one item in activity_home) new class: StaggeredRecyclerViewAdapter
    Please rebuild the jump function between activity_Home and activity_register.

--- app/src/main/java/com/example/navigation/Menu.java
+++ app/src/main/java/com/example/navigation/Menu.java
@@ -2,6 +2,7 @@ package com.example.navigation;
 
 import java.util.*;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,9 +10,15 @@ import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -22,13 +29,18 @@ import okhttp3.*;
 import com.alibaba.fastjson.*;
 
 public class Menu extends AppCompatActivity {
-
+    Boolean isScrolling = false;
+    ProgressBar progressBar;
+    StaggeredRecyclerViewAdapter staggeredRecyclerViewAdapter;
+    int currentItems,totalItems,scrollOutItems;
+    private static final String TAG = "Menu";
+    private static final int NUM_COLUMNS = 2;
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private ArrayList<String> mNames = new ArrayList<>();
     private TextView mTextMessage;
-
     private BottomNavigationView navigation;
     private TextView recipeName;
     private ImageView imageView;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -48,31 +60,128 @@ public class Menu extends AppCompatActivity {
             return false;
         }
     };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        //mTextMessage = (TextView) findViewById(R.id.message);
-
-        loadHome();
-    }
-    private void loadHome() {
-
-        setContentView(R.layout.activity_home);
-        //mTextMessage = (TextView) findViewById(R.id.message);
-        recipeName = findViewById(R.id.textView1);
         navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        imageView = findViewById(R.id.imageView1);
-        imageView.setOnClickListener(new View.OnClickListener() {
+        //mTextMessage = (TextView) findViewById(R.id.message);
+        initImageBitmaps();
+        //loadHome();
+    }
+    private void initImageBitmaps(){
+        Log.d(TAG,"initImageBitmaps: preparing bitmaps.");
+        //Here to process your JSON, Leader.
+        mImageUrls.add("https://assets3.thrillist.com/v1/image/2797371/size/tmg-article_default_mobile.jpg");
+        mNames.add("Hamberger");
+        mImageUrls.add("https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/one_pot_chorizo_and_15611_16x9.jpg");
+        mNames.add("noodles");
+        mImageUrls.add("https://www.bbcgoodfood.com/sites/default/files/guide/guide-image/2018/06/chicken-wings-main.jpg");
+        mNames.add("Chicken Wings");
+        mImageUrls.add("https://assets3.thrillist.com/v1/image/2815308/size/tmg-article_default_mobile.jpg");
+        mNames.add("Fried Chiken");
+        mImageUrls.add("https://wi-images.condecdn.net/image/V1RQ0lZ8pgQ/crop/3240/f/istock-459025743.jpg");
+        mNames.add("Lunch Meat");
+        mImageUrls.add("https://www.godairyfree.org/wp-content/uploads/2017/11/fast-food-feature-2.jpg");
+        mNames.add("French fries");
+        mImageUrls.add("https://2q0p8d1c7z0l2s9xl3323ez3-wpengine.netdna-ssl.com/wp-content/uploads/2018/04/Ultimate-Tokyo-Japan-Food-Guide-Featured.png");
+        mNames.add("海鮮丼");
+        mImageUrls.add("https://assets3.thrillist.com/v1/image/2797371/size/tmg-article_default_mobile.jpg");
+        mNames.add("Hamberger");
+        mImageUrls.add("https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/one_pot_chorizo_and_15611_16x9.jpg");
+        mNames.add("noodles");
+        mImageUrls.add("https://www.bbcgoodfood.com/sites/default/files/guide/guide-image/2018/06/chicken-wings-main.jpg");
+        mNames.add("Chicken Wings");
+        mImageUrls.add("https://assets3.thrillist.com/v1/image/2815308/size/tmg-article_default_mobile.jpg");
+        mNames.add("Fried Chiken");
+        mImageUrls.add("https://wi-images.condecdn.net/image/V1RQ0lZ8pgQ/crop/3240/f/istock-459025743.jpg");
+        mNames.add("Lunch Meat");
+        mImageUrls.add("https://www.godairyfree.org/wp-content/uploads/2017/11/fast-food-feature-2.jpg");
+        mNames.add("French fries");
+        mImageUrls.add("https://2q0p8d1c7z0l2s9xl3323ez3-wpengine.netdna-ssl.com/wp-content/uploads/2018/04/Ultimate-Tokyo-Japan-Food-Guide-Featured.png");
+        mNames.add("海鮮丼");
+        initRecyclerView();
+    }
+
+    private void loadmoreBitmaps(){
+        Log.d(TAG,"loadmoreBitmaps: preparing bitmaps.");
+        progressBar.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Menu.this, Recipe.class);
-                startActivity(intent);
-                System.out.println("click pic");
+            public void run() {
+                //Here to process your JSON, Leader.
+                mImageUrls.add("https://assets3.thrillist.com/v1/image/2797371/size/tmg-article_default_mobile.jpg");
+                mNames.add("Hamberger");
+                mImageUrls.add("https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/one_pot_chorizo_and_15611_16x9.jpg");
+                mNames.add("noodles");
+                mImageUrls.add("https://www.bbcgoodfood.com/sites/default/files/guide/guide-image/2018/06/chicken-wings-main.jpg");
+                mNames.add("Chicken Wings");
+                mImageUrls.add("https://assets3.thrillist.com/v1/image/2815308/size/tmg-article_default_mobile.jpg");
+                mNames.add("Fried Chiken");
+                mImageUrls.add("https://wi-images.condecdn.net/image/V1RQ0lZ8pgQ/crop/3240/f/istock-459025743.jpg");
+                mNames.add("Lunch Meat");
+                mImageUrls.add("https://www.godairyfree.org/wp-content/uploads/2017/11/fast-food-feature-2.jpg");
+                mNames.add("French fries");
+                mImageUrls.add("https://2q0p8d1c7z0l2s9xl3323ez3-wpengine.netdna-ssl.com/wp-content/uploads/2018/04/Ultimate-Tokyo-Japan-Food-Guide-Featured.png");
+                mNames.add("海鮮丼");
+                staggeredRecyclerViewAdapter.notifyDataSetChanged();
+                progressBar.setVisibility(View.GONE);
+            }
+        },2000);
+    }
+    private void initRecyclerView(){
+        Log.d(TAG,"initRecyclerView: initializing staggered recyclerview");
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        progressBar = (ProgressBar) findViewById(R.id.progress);
+        staggeredRecyclerViewAdapter =
+                new StaggeredRecyclerViewAdapter(this,mNames,mImageUrls);
+        final StaggeredGridLayoutManager staggeredGridLayoutManager =
+                new StaggeredGridLayoutManager(NUM_COLUMNS, LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+        recyclerView.setAdapter(staggeredRecyclerViewAdapter);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL)
+                {
+                    isScrolling = true;
+                }
+            }
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                currentItems = staggeredGridLayoutManager.getChildCount();
+                totalItems = staggeredGridLayoutManager.getItemCount();
+                int[] firstVisibleItem=null;
+                firstVisibleItem = staggeredGridLayoutManager.findFirstCompletelyVisibleItemPositions(firstVisibleItem);
+                if(firstVisibleItem != null && firstVisibleItem.length > 0) {
+                    scrollOutItems = firstVisibleItem[0];
+                }
+                if(isScrolling && (currentItems + scrollOutItems == totalItems)){
+                    //data fetch here, leader
+                    isScrolling=false;
+                    loadmoreBitmaps();
+                }
             }
         });
+    }
+    private void loadHome() {
+        //setContentView(R.layout.activity_home);
+        //mTextMessage = (TextView) findViewById(R.id.message);
+        //recipeName = findViewById(R.id.textView1);
+        //navigation = findViewById(R.id.navigation);
+        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//        imageView = findViewById(R.id.imageView1);
+//        imageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Menu.this, Recipe.class);
+//                startActivity(intent);
+//                System.out.println("click pic");
+//            }
+//        });
         getPhotoName();
         System.out.println("load home");
     }
