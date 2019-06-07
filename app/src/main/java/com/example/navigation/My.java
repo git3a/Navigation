860a8337baab3344bd02215f9bee0f659ae4bea3
sun hong <git3agroup@gami.com>
2019-06-07 14:46:19 +0900

    add logout

--- app/src/main/java/com/example/navigation/My.java
+++ app/src/main/java/com/example/navigation/My.java
@@ -1,11 +1,14 @@
 package com.example.navigation;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +16,7 @@ public class My extends AppCompatActivity {
     private BottomNavigationView navigation;
     private TextView recipeName;
     private ImageView imageView;
+    private Button logout;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -44,8 +48,21 @@ public class My extends AppCompatActivity {
         setContentView(R.layout.activity_my);
         navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        logout = findViewById(R.id.logOutButton);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getSharedPreferences("userinfo", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.commit();
+                Intent intent = new Intent(My.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
         loadRecipe();
     }
+
     private void loadRecipe() {
     }
 }
